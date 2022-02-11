1312f07fee4f23f04c722fecae2263a683c37e08
Muhammad Shafeeq <madrasaacc@gmail.com>
2022-02-11 17:35:03 +0400

    commit message

--- SafrDubai/routes.py
+++ SafrDubai/routes.py
@@ -9,22 +9,26 @@ def route():
     if request.method == "POST":
         starting_point = request.form["starting"]
         destination = request.form["destination"]
-        starting_point = geocode(starting_point)
-        destination = geocode(destination)
-        if starting_point == "An Error Occurred":
-            flash("Unable to find route", "failure")
+        try:
+            starting_point = geocode(starting_point)
+            destination = geocode(destination)
+            if starting_point == "An Error Occurred":
+                flash("Unable to find route", "failure")
+                return render_template("form.html")
+            else:
+                route = transit(starting_point[0], starting_point[1], destination[0], destination[1])
+                try:
+                    if route["notices"][0]["code"] == "noCoverage":
+                        flash("Routing is not possible due to missing information. (Try again with full address)")
+                        return render_template("form.html")
+                except:
+                    for item in route["routes"][0]["sections"]:
+                        item["departure"]["time"] = fix_time(item["departure"]["time"])
+                        item["arrival"]["time"] = fix_time(item["arrival"]["time"])
+                    return render_template("transit.html", route=route)
+        except:
+            flash("Routing is not possible due to missing information. (Try again with full address)")
             return render_template("form.html")
-        else:
-            route = transit(starting_point[0], starting_point[1], destination[0], destination[1])
-            try:
-                if route["notices"][0]["code"] == "noCoverage":
-                    flash("Routing is not possible due to missing information. (Try again with full address)")
-                    return render_template("form.html")
-            except:
-                for item in route["routes"][0]["sections"]:
-                    item["departure"]["time"] = fix_time(item["departure"]["time"])
-                    item["arrival"]["time"] = fix_time(item["arrival"]["time"])
-                return render_template("transit.html", route=route)
     return render_template('form.html')
 
 @app.route('/')
